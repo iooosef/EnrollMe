db999dec1ab0dab3eb7295a24e15a708519c0b35
iooosef <josence22@gmail.com>
2023-11-11 21:31:13 +0800

    Added handling of templates dir in cmake

--- src/EnrollMe.cpp
+++ src/EnrollMe.cpp
@@ -8,8 +8,14 @@ int main()
     crow::SimpleApp app; //define your crow application
 
     //define your endpoint at the root directory
-    CROW_ROUTE(app, "/")([]() {
-        return "Hello world";
+    //CROW_ROUTE(app, "/")([]() {
+    //    return "Hello world";
+    //    });
+
+    CROW_ROUTE(app, "/<string>")([](std::string name) { 
+        auto page = crow::mustache::load("index.html");
+        crow::mustache::context ctx({ {"person", name} }); 
+        return page.render(ctx); 
         });
 
     //set the port, set the app to run on multiple threads, and run the app
