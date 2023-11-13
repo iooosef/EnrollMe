6f87d69e24dec2ab212411bf8542bb4525904203
iooosef <josence22@gmail.com>
2023-11-13 11:18:14 +0800

    Added htmx.min.js

--- src/EnrollMe.cpp
+++ src/EnrollMe.cpp
@@ -12,19 +12,19 @@ int main()
     //    return "Hello world";
     //    });
 
-    //CROW_ROUTE(app, "/<string>")([](std::string name) { 
-    //    auto page = crow::mustache::load("index.html");
-    //    crow::mustache::context ctx({ {"person", name} }); 
-    //    return page.render(ctx); 
-    //    });
-
-    CROW_ROUTE(app, "/htmx/")(
-        []() {
-            crow::response myResponse(200, "<h1>test</h1><i>hello crow!</i>");  // Example constructor call
-            myResponse.set_header("Content-Type", "text/html");
-            myResponse.add_header("Custom-Header", "Custom-Value");
-            return myResponse;
+    CROW_ROUTE(app, "/<string>")([](std::string name) { 
+        auto page = crow::mustache::load("index.html");
+        crow::mustache::context ctx({ {"person", name} }); 
+        return page.render(ctx); 
         });
+
+    //CROW_ROUTE(app, "/htmx/")(
+    //    []() {
+    //        crow::response myResponse(200, "<h1>test</h1><i>hello crow!</i>");  // Example constructor call
+    //        myResponse.set_header("Content-Type", "text/html");
+    //        myResponse.add_header("Custom-Header", "Custom-Value");
+    //        return myResponse;
+    //    });
 
     //set the port, set the app to run on multiple threads, and run the app
     app.bindaddr("127.0.0.1")
