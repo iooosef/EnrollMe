99608a63eec766eba486786fd905e67eae5f9c16
iooosef <josence22@gmail.com>
2023-11-27 17:32:58 +0800

    split units to optional lecture and lab units

--- src/models/tbl_curriculum.h
+++ src/models/tbl_curriculum.h
@@ -1,10 +1,12 @@
 #pragma once
+#include <optional>
 
 struct tbl_curriculum {
 	int id;
 	std::string course_code;
 	std::string course_title;
-	int units;
+	std::optional<int> lecture_units;
+	std::optional<int> lab_units;
 	std::optional<std::string> prerequisite;
 	std::optional<std::string> corequisite;
 	std::optional<std::string> metadata;
