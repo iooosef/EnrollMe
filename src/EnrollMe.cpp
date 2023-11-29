e11e0834371c1bd4d5403772e994d2c53841f9fc
iooosef <josence22@gmail.com>
2023-11-29 20:51:35 +0800

    Added cli arguments for main app

--- src/EnrollMe.cpp
+++ src/EnrollMe.cpp
@@ -7,56 +7,92 @@ crow::response redirect()
     return res;
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    using Session = crow::SessionMiddleware<crow::FileStore>;
-    crow::App<crow::CookieParser, Session> EnrollMeApp
+    const char* dbPath = "./data/EnrollMe.db";
+    if (argc >= 2)
     {
-        Session
+        if (strcmp(argv[1], "initdb") == 0)
         {
-            // cookies cusomization
-            crow::CookieParser::Cookie("session")
-                .max_age(24 * 60 * 60) // 1 day
-                .path("/"),
-            4, // session Id length
-            crow::FileStore{"/"}
+            // initialize the database
+            Database EnrollMeDb(dbPath);
+            if (EnrollMeDb.openDB())
+            {
+                if (EnrollMeDb.initEnrollMeTables())
+                {
+					std::cout << "EnrollMe database initialized successfully." << std::endl;
+                    EnrollMeDb.closeDB();
+					return 0;
+				}
+                EnrollMeDb.closeDB();
+            }
+			return 0;
+		}
+        else if (strcmp(argv[1], "load-curriculum") == 0)
+        {
+			// seed the database
+			Database EnrollMeDb(dbPath);
+            if (EnrollMeDb.openDB())
+            {
+				if (EnrollMeDb.loadCurriculum())
+				{
+                    std::cout << "The \"tbl_curriculum.csv\" has been loaded into database successfully." << std::endl;
+                    EnrollMeDb.closeDB();
+                    return 0;
+                }
+                EnrollMeDb.closeDB();
+            }
+            return 0;
         }
-    };
+        else if (strcmp(argv[1], "seeddb") == 0)
+        {
+			// seed the database
+			Database EnrollMeDb(dbPath);
+            if (EnrollMeDb.openDB())
+            {
+                if (EnrollMeDb.seedDatabase())
+                {
+					std::cout << "EnrollMe database seeded successfully." << std::endl;
+					EnrollMeDb.closeDB();
+					return 0;
+				}
+				EnrollMeDb.closeDB();
+			}
+			return 0;
+		}
+        else if (strcmp(argv[1], "run") == 0)
+        {
+            // add the main app here later
+            std::cout << "Running EnrollMe..." << std::endl;
+            using Session = crow::SessionMiddleware<crow::FileStore>;
+            crow::App<crow::CookieParser, Session> EnrollMeApp
+            {
+                Session
+                {
+                    // cookies cusomization
+                    crow::CookieParser::Cookie("session")
+                        .max_age(24 * 60 * 60) // 1 day
+                        .path("/"),
+                    4, // session Id length
+                    crow::FileStore{"/"}
+                }
+            };
 
-    // Controller objects declaration
-    Home home;
-    Enroll enroll;
-    Logging log;
-    
-    
-    home.include_routes(EnrollMeApp);
-    enroll.include_routes(EnrollMeApp);
-    log.include_routes(EnrollMeApp);
-    
+            // Controller objects declaration
+            Home home;
+            Enroll enroll;
+            Logging log;
 
-    //set the port, set the app to run on multiple threads, and run the app
-    EnrollMeApp.bindaddr("127.0.0.1")
-        .port(18080)
-        .multithreaded()
-        .run_async();
+            home.include_routes(EnrollMeApp);
+            enroll.include_routes(EnrollMeApp);
+            log.include_routes(EnrollMeApp);
+
+
+            //set the port, set the app to run on multiple threads, and run the app
+            EnrollMeApp.bindaddr("127.0.0.1")
+                .port(18080)
+                .multithreaded()
+                .run_async();
+        }
+    }
 }
-
-
-//define your endpoint at the root directory
-/*CROW_ROUTE(app, "/")([]() {
-    return "Hello world";
-    });
-
-CROW_ROUTE(app, "/<string>")([](std::string name) {
-    auto page = crow::mustache::load("index.html");
-    crow::mustache::context ctx({ {"person", name} });
-    return page.render(ctx);
-    });
-
-CROW_ROUTE(app, "/htmx/")(
-    []() {
-        crow::response myResponse(200, "<h1>test</h1><i>hello crow!</i>");
-        myResponse.set_header("Content-Type", "text/html");
-        myResponse.add_header("Custom-Header", "Custom-Value");
-        return myResponse;
-    });*/
